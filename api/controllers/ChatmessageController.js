ded43ea59b276584e2c305cbd52870ee311cada8
ElenaOwa <len.owa@yandex.by>
2017-11-08 14:53:06 +0300

    form and api create

---
+++ api/controllers/ChatmessageController.js
@@ -0,0 +1,11 @@
+/**
+ * ChatmessageController
+ *
+ * @description :: Server-side logic for managing chatmessages
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
